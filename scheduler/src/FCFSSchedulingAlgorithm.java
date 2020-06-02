7699e63bf0b76b6bf5a4a184afc7844c0f5ecb04
Marco Canto <mako.canto@gmail.com>
2020-06-02 14:25:52 -0700

    FCFS

--- scheduler/src/FCFSSchedulingAlgorithm.java
+++ scheduler/src/FCFSSchedulingAlgorithm.java
@@ -11,62 +11,57 @@ import java.util.*;
 
 public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
 
+    private ArrayList<Process> jobs;
+    
+    class FCFSComparator implements Comparator<Process> {
+    	public int compare(Process p1, Process p2) {
+    		if(p1.getArrivalTime() != p2.getArrivalTime()) {
+    			return Long.signum(p1.getArrivalTime() - p2.getArrivalTime());
+    		}
+    		return Long.signum(p1.getPID() - p2.getPID());
+    	}
+    }
+    
+    FCFSComparator comparator = new FCFSComparator();
+
     FCFSSchedulingAlgorithm(){
-        // Fill in this method
-        /*------------------------------------------------------------*/
-
-
-
-        /*------------------------------------------------------------*/
+		activeJob = null;
+		jobs = new ArrayList<Process>();
     }
 
     /** Add the new job to the correct queue.*/
     public void addJob(Process p){
-        // Remove the next lines to start your implementation
-        throw new UnsupportedOperationException();
-        
-        // Fill in this method
-        /*------------------------------------------------------------*/
-
-
-
-        /*------------------------------------------------------------*/
+    	jobs.add(p);
+    	Collections.sort(jobs,comparator);
     }
     
     /** Returns true if the job was present and was removed. */
     public boolean removeJob(Process p){
-        // Remove the next lines to start your implementation
-        throw new UnsupportedOperationException();
-
-        // Fill in this method
-        /*------------------------------------------------------------*/
-
-
-
-        /*------------------------------------------------------------*/
+		if(p == activeJob)
+		    activeJob = null;
+		return jobs.remove(p);
     }
 
     /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
-    when switching to another algorithm in the GUI */
+	when switching to another algorithm in the GUI */
     public void transferJobsTo(SchedulingAlgorithm otherAlg) {
-        throw new UnsupportedOperationException();
+    	throw new UnsupportedOperationException();
     }
 
     /** Returns the next process that should be run by the CPU, null if none available.*/
     public Process getNextJob(long currentTime){
-        // Remove the next lines to start your implementation
-        throw new UnsupportedOperationException();
-        
-        // Fill in this method
-        /*------------------------------------------------------------*/
-
-
-
-        /*------------------------------------------------------------*/
+    	Process earliest = null;
+    	
+    	if(!isJobFinished())
+    		return activeJob;
+		if(jobs.size() > 0)
+			earliest = jobs.get(0);
+    	activeJob = earliest;
+    	return activeJob;
     }
 
     public String getName(){
-        return "First-Come First-Served";
+    	return "First-Come First-Served";
     }
     
 }
